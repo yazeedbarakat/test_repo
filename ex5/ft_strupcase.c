7cdb0e8e656865e8024d131c8ae56b502810c9e3
yazeedbarakat <138019642+yazeedbarakat@users.noreply.github.com>
2025-09-16 22:48:15 +0300

    404

---
+++ ex5/ft_strupcase.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+char	*ft_strupcase(char *str)
+{	
+	int	i;
+
+	i = 0;
+	while(str[i])
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
+
+int	main()
+{
+	char	*x;
+	char	s[] = "df5A7grg";
+	x = ft_strupcase(s);
+	printf("%s",x);
+	return (0);
+}
